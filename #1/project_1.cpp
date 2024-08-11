deedaae05e63f85c029b409b076bed226f9ef952
reeuze <adittitik@gmail.com>
2024-08-11 19:46:10 +0700

    first commit

---
+++ #1/project_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Number, Sum;
+    for (int Number=0; Number<1000; Number++)
+    {
+        if (Number%3==0 || Number%5==0)
+        {
+            Sum = Sum + Number;
+        }
+    }
+    cout << "Sum : " << Sum << endl;
+    system("pause");
+    return 0;
+}
