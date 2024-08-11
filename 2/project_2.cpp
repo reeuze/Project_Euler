6541342d2212525ea6dca17af1fb74b58a89ab24
reeuze <adittitik@gmail.com>
2024-08-11 20:12:45 +0700

    Commit 2

--- 2/project_2.cpp
+++ 2/project_2.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int main()
 {
-    int F_Number = 1;
-    while (1)
+    int Now = 1, Before = 0, After = 0, Sum = 0;
+    while (Now < 4000000)
     {
-        if (F_Number == 1) 
+        if (Now == 1) Before = 1;
+        if (Now % 2 == 0) Sum = Sum + Now;
+        After = Before + Now;
+        // Shifting
+        Before = Now;
+        Now = After;
+        // Check
+        cout << "Fibonanci Currect : " << Now << " Sum Current : " << Sum << endl; 
     }
+    cout << "Sum Even Number : " << Sum << endl;
     system("pause");
     return 0;
 }
